e2aa108f2effe161007f6241a77027227d02a4f3
pemila <pemila@126.com>
2019-08-30 10:36:30 +0800

    add 使用new AssetImage加载图片

--- lib/assetsPage.dart
+++ lib/assetsPage.dart
@@ -1,13 +1,11 @@
 import 'dart:async';
-import 'dart:collection';
-
+import 'dart:ui' as prefix0;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class AssetsPageRoute extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
         appBar: AppBar(title: Text("资源展示"),),
         body: Center(
@@ -15,7 +13,6 @@ class AssetsPageRoute extends StatelessWidget{
         ),
     );
   }
-
 }
 
 class AssetsPageWidget extends StatelessWidget{
@@ -23,13 +20,25 @@ class AssetsPageWidget extends StatelessWidget{
     Future<String>  loadAsset() async {
         return await rootBundle.loadString("assets/text.txt");
     }
-    @override
+/*    @override
     Widget build(BuildContext context) {
         loadAsset().then((txt)=> print(txt) );
         return Column(
             children: <Widget>[
                 // 加载图片获得widget
                 Image.asset("assets/icon1.png"),
+            ],
+        );
+    }*/
+
+/*  使用 new AssetImage加载图片*/
+    @override
+    Widget build(BuildContext context){
+        loadAsset().then((txt)=> print(txt) );
+        AssetImage assetImage = new AssetImage("assets/icon2.png");
+        return Column(
+            children: <Widget>[
+                new Image(image: assetImage,width: 200,height: 500,),
             ],
         );
     }
