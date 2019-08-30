f00d136dae9a6223fc2b460e03e6dd11c8388ce8
pemila <pemila@126.com>
2019-08-30 15:32:46 +0800

    add 日志输出工具类

--- android/app/src/main/java/com/pemila/my_flutter_app/MainActivity.java
+++ android/app/src/main/java/com/pemila/my_flutter_app/MainActivity.java
@@ -1,7 +1,11 @@
 package com.pemila.my_flutter_app;
 
 import android.os.Bundle;
+import android.util.Log;
+
 import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
@@ -9,5 +13,23 @@ public class MainActivity extends FlutterActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+    new MethodChannel(getFlutterView(),"android_log")
+            .setMethodCallHandler(
+                    (methodCall, result) -> logPrint(methodCall)
+            );
+  }
+
+  private void logPrint(MethodCall methodCall) {
+    String tag = methodCall.argument("tag");
+    String message = methodCall.argument("msg");
+    assert message != null;
+    switch (methodCall.method) {
+      case "logV":  Log.v(tag, message); break;
+      case "logD":  Log.d(tag, message); break;
+      case "logI":  Log.i(tag, message); break;
+      case "logW":  Log.w(tag, message); break;
+      default:
+        Log.e(tag, message);
+    }
   }
 }
